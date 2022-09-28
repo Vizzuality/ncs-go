21bbb37aabf35c41133b764caf4d2afcedc9e0ea
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-09-28 23:59:18 +0200

    Circles using particles

--- src/containers/home/particles/component.tsx
+++ src/containers/home/particles/component.tsx
@@ -1,15 +1,24 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
 
 import Logo from './logo';
 
 const Particles = () => {
   return (
     <div className="w-screen h-screen" style={{ background: '#011426' }}>
-      <Canvas camera={{ position: [20, 0, 0], fov: 75 }}>
-        <OrbitControls />
+      <Canvas
+        orthographic
+        camera={{ zoom: 100 }}
+        gl={{ antialias: true, toneMapping: THREE.NoToneMapping }}
+      >
+        {/* Light */}
         <ambientLight />
 
+        {/* Controls */}
+        <OrbitControls />
+
+        {/* Meshes */}
         <Logo />
       </Canvas>
     </div>
--- src/containers/home/particles/logo/component.tsx
+++ src/containers/home/particles/logo/component.tsx
@@ -1,76 +1,37 @@
-import { useMemo, useRef } from 'react';
+import Circles from './circles';
 
-import { useTexture } from '@react-three/drei';
-import { PointsProps, useFrame } from '@react-three/fiber';
-
-const COLORS = ['#02C6EC', '#FFFFFF'];
-
-const hex2rgb = (hex) => {
-  const r = parseInt(hex.slice(1, 3), 16) / 255;
-  const g = parseInt(hex.slice(3, 5), 16) / 255;
-  const b = parseInt(hex.slice(5, 7), 16) / 255;
-  // return {r, g, b} // return an object
-  return [r, g, b];
-};
 const ParticlesLogo = () => {
-  const pointsRef = useRef<PointsProps>();
-  const count = 100;
-
-  const { positions, colors } = useMemo(() => {
-    let pos = [];
-    let clrs = [];
-
-    for (let i = 0; i < count; i++) {
-      let x = (i % 10) - 5;
-      let y = Math.floor(i / 10) - 5;
-      let z = Math.random() * 5 - 2.5;
-      let color = COLORS[Math.floor(Math.random() * 2)];
-      pos.push(x, y, z);
-      clrs.push(...hex2rgb(color));
-    }
-    return {
-      positions: new Float32Array(pos),
-      colors: new Float32Array(clrs),
-    };
-  }, [count]);
-
-  const CIRCLE_PNG = useTexture('/images/circle.png');
-
-  useFrame(() => {
-    if (pointsRef.current) {
-      pointsRef.current.rotation.y += 0.005;
-    }
-  });
-
-  console.log(colors);
-
   return (
-    <points ref={pointsRef}>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          array={positions}
-          count={positions.length / 3} //
-          itemSize={3}
-        />
-        <bufferAttribute
-          attach="attributes-color"
-          array={colors}
-          count={colors.length / 3} //
-          itemSize={3}
-        />
-      </bufferGeometry>
-
-      <pointsMaterial
-        map={CIRCLE_PNG}
-        vertexColors
-        size={0.5}
-        sizeAttenuation
-        transparent={false}
-        alphaTest={0.5}
-        opacity={1}
+    <>
+      <Circles //
+        count={12}
+        size={10}
+        color={[1, 1, 1]}
+        radius={40}
+        startAngle={0}
       />
-    </points>
+      <Circles //
+        count={12}
+        size={5}
+        color={[0, 121 / 255, 208 / 255]}
+        radius={60}
+        startAngle={45}
+      />
+      <Circles
+        count={12}
+        size={10}
+        color={[2 / 255, 198 / 255, 236 / 255]}
+        radius={80}
+        startAngle={0}
+      />
+      <Circles
+        count={12}
+        size={5}
+        color={[29 / 255, 234 / 255, 189 / 255]}
+        radius={100}
+        startAngle={45}
+      />
+    </>
   );
 };
 
