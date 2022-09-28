fb2fc6507877d800110ef8172af049c7029b3c0a
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-09-29 00:25:00 +0200

    Circles using plane geometry

--- src/containers/home/particles/logo/circles/component.tsx
+++ src/containers/home/particles/logo/circles/component.tsx
@@ -1,63 +1,38 @@
 import { useMemo } from 'react';
 
-import { useTexture } from '@react-three/drei';
-
 export interface CirclesProps {
   count: number;
   radius: number;
-  color: [number, number, number];
+  color: number;
   size: number;
   startAngle?: number;
 }
 
 const Circles = ({ count, radius, color, size, startAngle }: CirclesProps) => {
-  const { positions, colors } = useMemo(() => {
+  const { positions } = useMemo(() => {
     let pos = [];
-    let clrs = [];
 
     for (let i = 0; i < count; i++) {
       const angle = i * (360 / count) - 90 + startAngle;
       const x = (radius / 100) * Math.cos((-angle * Math.PI) / 180);
       const y = (radius / 100) * Math.sin((-angle * Math.PI) / 180);
       const z = 0;
-      pos.push(x, y, z);
-      clrs.push(...color);
+      pos.push({ x, y, z });
     }
     return {
-      positions: new Float32Array(pos),
-      colors: new Float32Array(clrs),
+      positions: pos,
     };
-  }, [count, radius, color, startAngle]);
-
-  const CIRCLE_PNG = useTexture('/images/circle.png');
+  }, [count, radius, startAngle]);
 
   return (
-    <points>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          array={positions}
-          count={positions.length / 3} //
-          itemSize={3}
-        />
-        <bufferAttribute
-          attach="attributes-color"
-          array={colors}
-          count={colors.length / 3} //
-          itemSize={3}
-        />
-      </bufferGeometry>
-
-      <pointsMaterial
-        map={CIRCLE_PNG}
-        vertexColors
-        size={size}
-        sizeAttenuation
-        transparent={false}
-        alphaTest={0.1}
-        opacity={1}
-      />
-    </points>
+    <>
+      {positions.map((p) => (
+        <mesh key={`${p.x}-${p.y}-${p.z}`} position={[p.x, p.y, p.z]}>
+          <circleGeometry args={[size / 100, 32]} />
+          <meshBasicMaterial color={color} />
+        </mesh>
+      ))}
+    </>
   );
 };
 
--- src/containers/home/particles/logo/component.tsx
+++ src/containers/home/particles/logo/component.tsx
@@ -3,34 +3,11 @@ import Circles from './circles';
 const ParticlesLogo = () => {
   return (
     <>
-      <Circles //
-        count={12}
-        size={10}
-        color={[1, 1, 1]}
-        radius={40}
-        startAngle={0}
-      />
-      <Circles //
-        count={12}
-        size={5}
-        color={[0, 121 / 255, 208 / 255]}
-        radius={60}
-        startAngle={45}
-      />
-      <Circles
-        count={12}
-        size={10}
-        color={[2 / 255, 198 / 255, 236 / 255]}
-        radius={80}
-        startAngle={0}
-      />
-      <Circles
-        count={12}
-        size={5}
-        color={[29 / 255, 234 / 255, 189 / 255]}
-        radius={100}
-        startAngle={45}
-      />
+      <Circles count={1} size={10} color={0xfc1a52} radius={0} startAngle={0} />
+      <Circles count={12} size={5} color={0xffffff} radius={40} startAngle={0} />
+      <Circles count={12} size={2.5} color={0x0079d0} radius={60} startAngle={45} />
+      <Circles count={12} size={5} color={0x02c6ec} radius={80} startAngle={0} />
+      <Circles count={12} size={2.5} color={0x1deabd} radius={100} startAngle={45} />
     </>
   );
 };
