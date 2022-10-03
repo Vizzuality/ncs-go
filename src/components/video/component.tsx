fb678d7c7e4099ff34e0dc80433b256fa6b3d716
anamontiaga <amontiaga@gmail.com>
2022-10-03 11:10:29 +0200

    add play control custom ion

--- src/components/video/component.tsx
+++ src/components/video/component.tsx
@@ -4,7 +4,22 @@ import cx from 'classnames';
 
 import ReactPlayer from 'react-player/lazy';
 
+import Icon from 'components/icon';
+
+import PLAY_SVG from 'svgs/play.svg?sprite';
+
 import type { VideoProps } from './types';
+
+const PlayControl = () => {
+  return (
+    <div className="relative">
+      <div className="flex w-40 h-40 rounded-full opacity-25 bg-brand"></div>
+      <div className="absolute z-10 flex items-center justify-center w-32 h-32 rounded-full bg-brand top-4 left-4">
+        <Icon icon={PLAY_SVG} className="inline-block w-5 h-8 text-white bg-brand" />
+      </div>
+    </div>
+  );
+};
 
 export const Video: FC<VideoProps> = ({
   className,
@@ -25,6 +40,7 @@ export const Video: FC<VideoProps> = ({
     light={light}
     playing={playing}
     config={config}
+    playIcon={<PlayControl />}
   />
 );
 
