023704127250da084ac2cd031da4415d5015ec3b
anamontiaga <amontiaga@gmail.com>
2022-10-03 11:39:33 +0200

    types

--- src/components/video/types.d.ts
+++ src/components/video/types.d.ts
@@ -1,17 +1,5 @@
-export interface VideoProps {
+import type { ReactPlayerProps } from 'react-player';
+
+export interface VideoProps extends ReactPlayerProps {
   className?: string;
-
-  // * Override options for the various players
-  config?: Record<string, unknown>;
-
-  // * To loop the media
-  loop?: boolean;
-
-  // * To pause or play the media: https://github.com/cookpete/react-player#config-prop
-  playing?: boolean;
-
-  // * Show the video thumbnail, which loads the full player on click
-  light?: boolean;
-
-  url: string;
 }
