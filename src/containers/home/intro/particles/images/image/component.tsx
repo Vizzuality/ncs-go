368633fe82c884981c689febd335a29afc7a8bcf
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-10-05 17:07:52 +0200

    Animation: images

--- src/containers/home/intro/particles/images/image/component.tsx
+++ src/containers/home/intro/particles/images/image/component.tsx
@@ -5,18 +5,21 @@ import { useHomeStore } from 'store/home';
 import { useTexture } from '@react-three/drei';
 import { extend, useFrame } from '@react-three/fiber';
 import { motion } from 'framer-motion-3d';
+import { Color } from 'three';
 
-import TextureMaterial from './material';
+import { TextureMaterial, RingMaterial } from './materials';
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       // @ts-ignore
       textureMaterial: ReactThreeFiber.Object3DNode<TextureMaterial, typeof TextureMaterial>;
+      // @ts-ignore
+      ringMaterial: ReactThreeFiber.Object3DNode<RingMaterial, typeof RingMaterial>;
     }
   }
 }
-extend({ TextureMaterial });
+extend({ TextureMaterial, RingMaterial });
 
 export interface CircleImageProps {
   p: {
@@ -31,6 +34,8 @@ export interface CircleImageProps {
 
 const CircleImage = ({ p, radius, img }: CircleImageProps) => {
   const textureMaterialRef = useRef(null);
+  const ringMaterialRef = useRef(null);
+
   const step = useHomeStore((state) => state.step);
   const texture = useTexture(img);
 
@@ -40,35 +45,74 @@ const CircleImage = ({ p, radius, img }: CircleImageProps) => {
     return Math.random();
   }, []);
 
+  const U_COLOR = useMemo(() => {
+    return new Color(0xaaaaaa);
+  }, []);
+
   useFrame(() => {
     textureMaterialRef.current.uTime = performance.now() / 1000;
+    ringMaterialRef.current.uTime = performance.now() / 1000;
   });
 
   return (
     <motion.group
-      position-x={p.x}
-      position-y={p.y}
-      position-z={p.z}
-      initial={{ scale: 0 }}
-      animate={{ scale: step === 3 ? 1 : 0 }}
+      initial={{ x: 0, y: 0.75, z: 0, scale: 0 }}
+      animate={{
+        ...(step === 3 && {
+          x: p.x,
+          y: p.y,
+          z: p.z,
+          scale: 1,
+        }),
+
+        ...(step !== 3 && {
+          x: 0,
+          y: 0.75,
+          z: 0,
+          scale: 0,
+        }),
+      }}
       transition={{
-        delay: step === 3 ? 0.4 : 0,
+        duration: 0.5,
       }}
     >
-      <mesh>
-        <circleGeometry args={[radius / 100, 64]} />
-        <textureMaterial
-          ref={textureMaterialRef}
-          uTime={0}
-          uTexture={texture}
-          uVelocity={velocity}
-        />
-      </mesh>
+      <motion.group
+        initial={{ scale: 0.95 }}
+        animate={{ scale: 1.1 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: 'reverse',
+          duration: 10 + velocity * 10,
+        }}
+      >
+        <mesh>
+          <ringGeometry args={[ring / 100, (ring + ring * 0.01) / 100, 64]} />
+          <ringMaterial
+            ref={ringMaterialRef}
+            uTime={0}
+            uVelocity={velocity}
+            uNoise={0.1}
+            uRadius={radius / 100}
+            uCurrentPos={[p.x, p.y, p.z]}
+            uColor={U_COLOR}
+            transparent
+          />
+        </mesh>
 
-      <mesh>
-        <ringGeometry args={[ring / 100, (ring + ring * 0.01) / 100, 64]} />
-        <meshBasicMaterial color="#AAAAAA" />
-      </mesh>
+        <mesh>
+          <circleGeometry args={[radius / 100, 64]} />
+          <textureMaterial
+            ref={textureMaterialRef}
+            uTime={0}
+            uTexture={texture}
+            uVelocity={velocity}
+            uRadius={radius / 100}
+            uCurrentPos={[p.x, p.y, p.z]}
+            uNoise={0.1}
+            transparent
+          />
+        </mesh>
+      </motion.group>
     </motion.group>
   );
 };
