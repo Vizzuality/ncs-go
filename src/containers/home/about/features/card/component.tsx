0318c505eee8c681d09f770ad2a828cca0274b3c
anamontiaga <amontiaga@gmail.com>
2022-10-05 12:50:58 +0200

    fix responsive hero

--- src/containers/home/about/features/card/component.tsx
+++ src/containers/home/about/features/card/component.tsx
@@ -5,7 +5,7 @@ import Icon from 'components/icon';
 import type { FeatureCardProps } from './types';
 
 export const FeatureCard = ({ description, icon }: FeatureCardProps) => (
-  <div className="p-10 space-y-2 text-base text-left text-white bg-gray-900 md:space-y-6 md:text-lg md:h-80 md:pt-20 outline outline-1 outline-gray-800">
+  <div className="p-10 space-y-2 text-base text-left text-white bg-gray-900 md:space-y-6 md:text-lg md:h-80 xl:h-96 md:pt-20 outline outline-1 outline-gray-800">
     <Icon className="w-12 h-12 md:w-16 md:h-16" icon={icon} />
     <h3 className="font-sans ">{description}</h3>
   </div>
--- src/containers/home/about/features/component.tsx
+++ src/containers/home/about/features/component.tsx
@@ -15,7 +15,7 @@ const Features = () => {
           subtitle="A home base for all decision-makers interested in unlocking the power of nature to preserve our living planet."
         />
       </div>
-      <div className="md:grid md:grid-flow-col md:grid-rows-2 md:px-24">
+      <div className="grid md:grid-flow-col md:grid-rows-4 xl:grid-rows-2 md:px-24">
         {FEATURES.map((f) => (
           <div key={f.key} className={`order-${f.key}`}>
             <FeatureCard description={f.description} icon={f.icon} />
