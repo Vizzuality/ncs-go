3f137ed2257cc083bb23ecbaffa6ea7812442e87
anamontiaga <amontiaga@gmail.com>
2022-10-05 19:38:55 +0200

    improve naming

--- src/containers/home/stories/component.tsx
+++ src/containers/home/stories/component.tsx
@@ -5,7 +5,7 @@ import { useHomeStore } from 'store/home';
 import { useInView } from 'framer-motion';
 
 import Hero from 'containers/home/common/hero';
-import Cards from 'containers/home/stories/cards';
+import List from 'containers/home/stories/list';
 import Wrapper from 'containers/wrapper';
 
 const Stories = () => {
@@ -25,12 +25,23 @@ const Stories = () => {
         <div className="pb-20 pt-28">
           <Hero
             className="text-gray-800"
-            title="Inspiring stories"
-            subtitle="See how NCS projects on the ground have a positive impact on climate, biodiversity and people."
+            title="Stories"
+            subtitle="Here are some of the stories we have heard from the ground."
           />
         </div>
+        <div className="pb-10 font-sans text-base text-gray-800 md:text-lg md:grid md:grid-cols-3 md:gap-10">
+          <div className="col-start-1 pb-4 md:pb-0">
+            <p>Nature-based solutions are ready to be deployed right now, everywhere.</p>
+          </div>
+          <div className="col-start-2 space-y-4 md:space-y-6">
+            <p>
+              From south to north, east to west, people have been taking action and leading the way
+              to reverse nature loss and create new sustainable economies.
+            </p>
+          </div>
+        </div>
       </Wrapper>
-      <Cards />
+      <List />
     </div>
   );
 };
---
+++ src/containers/home/stories/list/component.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Hero from 'containers/home/common/hero';
+import StoryCard from 'containers/home/stories/list/story-card';
+
+import Button from 'components/button';
+
+import { STORIES } from './constants';
+
+const List = () => {
+  return (
+    <div className="bg-gray-900 md:pb-28">
+      <div className="px-4 py-10 md:pb-28 md:px-24">
+        <Hero className="text-white" subtitle="Want more examples?" />
+      </div>
+      <div className="md:grid md:grid-cols-12">
+        <div className="flex flex-col space-y-4 md:px-24 md:col-span-10 md:col-start-2">
+          {STORIES.map((s) => (
+            <div key={s.id}>
+              <StoryCard
+                articleUrl={s.articleUrl}
+                clipUrl={s.clipUrl}
+                country={s.country}
+                description={s.description}
+                image={s.image}
+                pathway={s.pathway}
+                title={s.title}
+                videoUrl={s.videoUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-10">
+        <Button
+          className="rounded-full"
+          theme="secondary"
+          size="xs"
+          onClick={() => console.info('Show more stories')}
+        >
+          Show more stories
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default List;
