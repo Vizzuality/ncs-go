17acfbda224e8ffac13705b530901d13a306257c
anamontiaga <amontiaga@gmail.com>
2022-10-06 18:14:44 +0200

    fix wrapper stories

--- src/containers/home/stories/list/component.tsx
+++ src/containers/home/stories/list/component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Hero from 'containers/home/common/hero';
 import StoryCard from 'containers/home/stories/list/story-card';
+import Wrapper from 'containers/wrapper';
 
 import Button from 'components/button';
 
@@ -9,38 +10,40 @@ import { STORIES } from './constants';
 
 const List = () => {
   return (
-    <div className="bg-gray-900 md:pb-28">
-      <div className="px-4 py-10 md:pb-28 md:px-24">
-        <Hero className="text-white" subtitle="Want more examples?" />
-      </div>
-      <div className="xl:grid xl:grid-cols-12">
-        <div className="flex flex-col px-4 space-y-4 md:px-24 xl:col-span-10 xl:col-start-2">
-          {STORIES.map((s) => (
-            <div key={s.id}>
-              <StoryCard
-                articleUrl={s.articleUrl}
-                clipUrl={s.clipUrl}
-                country={s.country}
-                description={s.description}
-                image={s.image}
-                pathway={s.pathway}
-                title={s.title}
-                videoUrl={s.videoUrl}
-              />
-            </div>
-          ))}
+    <div className="bg-gray-900 md:py-28">
+      <Wrapper>
+        <div className="py-10 md:pb-28">
+          <Hero className="text-white" subtitle="Want more examples?" />
         </div>
-      </div>
-      <div className="flex justify-center w-full py-10">
-        <Button
-          className="rounded-full"
-          theme="secondary"
-          size="xs"
-          onClick={() => console.info('Show more stories')}
-        >
-          Show more stories
-        </Button>
-      </div>
+        <div className="xl:grid xl:grid-cols-12">
+          <div className="flex flex-col space-y-4 xl:col-span-10 xl:col-start-2">
+            {STORIES.map((s) => (
+              <div key={s.id}>
+                <StoryCard
+                  articleUrl={s.articleUrl}
+                  clipUrl={s.clipUrl}
+                  country={s.country}
+                  description={s.description}
+                  image={s.image}
+                  pathway={s.pathway}
+                  title={s.title}
+                  videoUrl={s.videoUrl}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-center w-full py-10">
+          <Button
+            className="rounded-full"
+            theme="secondary"
+            size="xs"
+            onClick={() => console.info('Show more stories')}
+          >
+            Show more stories
+          </Button>
+        </div>
+      </Wrapper>
     </div>
   );
 };
