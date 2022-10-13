a484c459b2ab8ff56e345737c2ace11771c2a91a
anamontiaga <amontiaga@gmail.com>
2022-10-13 14:24:45 +0200

    animate sample cards

--- src/containers/home/stories/samples/sample-composition/component.tsx
+++ src/containers/home/stories/samples/sample-composition/component.tsx
@@ -19,15 +19,21 @@ const SampleComposition = ({ media, card, align = 'right' }) => (
         {media}
       </motion.div>
 
-      <div
+      <motion.div
         className={cx({
           'w-full lg:w-[408px] lg:absolute top-1/3': true,
           'left-full': align === 'left',
           'left-0': align === 'right',
         })}
+        initial={{ opacity: 0.5, x: align === 'left' ? 350 : -350 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.5,
+          delay: 0.25,
+        }}
       >
         <div className="w-full lg:max-w-[400px] lg:-translate-x-1/2">{card}</div>
-      </div>
+      </motion.div>
     </div>
   </div>
 );
