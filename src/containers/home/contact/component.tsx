3547d3da5e9d8d05f139e1435a2cbf33a049b0bb
anamontiaga <amontiaga@gmail.com>
2022-10-17 10:40:59 +0200

    fix reset form

--- src/containers/home/contact/component.tsx
+++ src/containers/home/contact/component.tsx
@@ -18,6 +18,7 @@ import { BREAKPOINTS } from 'styles/styles.config';
 
 const Contact: React.FC = () => {
   const ref = useRef();
+  const formRef = useRef(null);
   const [submitting, setSubmitting] = useState(false);
   const [toast, displayToast] = useState(false);
 
@@ -92,57 +93,60 @@ const Contact: React.FC = () => {
           </div>
 
           <Form initialValues={{ email: null }} onSubmit={onSubmit}>
-            {({ handleSubmit, form }) => (
-              <form
-                className="py-6 xl:col-span-6 xl:py-0"
-                noValidate
-                onSubmit={(event) => handleSubmit(event, form)}
-              >
-                <div className="flex flex-col justify-between w-full space-y-4 xl:flex-row xl:space-y-0">
-                  <Field
-                    name="email"
-                    component="input"
-                    validate={composeValidators([{ presence: true, email: true }])}
-                  >
-                    {({ input, meta }) => (
-                      <motion.div
-                        className="relative w-full"
-                        animate={{ opacity }}
-                        transition={{ delay: 0.4 }}
-                      >
-                        <input
-                          {...input}
-                          value={input.value as string}
-                          type="email"
-                          placeholder="Enter your email"
-                          className="flex w-full px-10 py-4 text-base transition duration-300 ease-in-out delay-150 bg-gray-100 border-none rounded-full focus:outline-none focus:ring-inset focus:ring-2 focus:ring-brand-700 focus:bg-white md:text-lg md:py-5 xl:rounded-l-full xl:rounded-r-none placeholder:text-gray-400"
-                        />
-                        {meta.error &&
-                          meta.touched &&
-                          meta.active &&
-                          minWidth >= BREAKPOINTS.xl && (
-                            <p className="absolute text-sm text-red-600 top-20 left-10">
-                              {capitalizeString(meta.error)}
-                            </p>
-                          )}
-                      </motion.div>
-                    )}
-                  </Field>
-
-                  <motion.div animate={{ opacity }} transition={{ delay: 0.5, bounce: 0 }}>
-                    <Button
-                      disabled={submitting}
-                      size="s"
-                      theme="primary"
-                      type="submit"
-                      className="w-full space-x-4 xl:rounded-r-full xl:rounded-l-none"
+            {({ handleSubmit, form }) => {
+              formRef.current = form;
+              return (
+                <form
+                  className="py-6 xl:col-span-6 xl:py-0"
+                  noValidate
+                  onSubmit={(event) => handleSubmit(event)}
+                >
+                  <div className="flex flex-col justify-between w-full space-y-4 xl:flex-row xl:space-y-0">
+                    <Field
+                      name="email"
+                      component="input"
+                      validate={composeValidators([{ presence: true, email: true }])}
                     >
-                      <p>Subscribe</p>
-                    </Button>
-                  </motion.div>
-                </div>
-              </form>
-            )}
+                      {({ input, meta }) => (
+                        <motion.div
+                          className="relative w-full"
+                          animate={{ opacity }}
+                          transition={{ delay: 0.4 }}
+                        >
+                          <input
+                            {...input}
+                            value={input.value as string}
+                            type="email"
+                            placeholder="Enter your email"
+                            className="flex w-full px-10 py-4 text-base transition duration-300 ease-in-out delay-150 bg-gray-100 border-none rounded-full focus:outline-none focus:ring-inset focus:ring-2 focus:ring-brand-700 focus:bg-white md:text-lg md:py-5 xl:rounded-l-full xl:rounded-r-none placeholder:text-gray-400"
+                          />
+                          {meta.error &&
+                            meta.touched &&
+                            meta.active &&
+                            minWidth >= BREAKPOINTS.xl && (
+                              <p className="absolute text-sm text-red-600 top-20 left-10">
+                                {capitalizeString(meta.error)}
+                              </p>
+                            )}
+                        </motion.div>
+                      )}
+                    </Field>
+
+                    <motion.div animate={{ opacity }} transition={{ delay: 0.5, bounce: 0 }}>
+                      <Button
+                        disabled={submitting}
+                        size="s"
+                        theme="primary"
+                        type="submit"
+                        className="w-full space-x-4 xl:rounded-r-full xl:rounded-l-none"
+                      >
+                        <p>Subscribe</p>
+                      </Button>
+                    </motion.div>
+                  </div>
+                </form>
+              );
+            }}
           </Form>
           {minWidth < BREAKPOINTS.xl && (
             <motion.p
