99c1581768115606a4023c576c0691a502aac9f7
Ana Montiaga <amontiaga@gmail.com>
2022-10-25 16:32:37 +0200

    feat: update features[NCS-19] (#25)

--- src/containers/home/about/features/constants.ts
+++ src/containers/home/about/features/constants.ts
@@ -1,51 +1,67 @@
-import FEATURE_1_SVG from 'svgs/feature-1.svg?sprite';
-import FEATURE_2_SVG from 'svgs/feature-2.svg?sprite';
-import FEATURE_3_SVG from 'svgs/feature-3.svg?sprite';
-import FEATURE_4_SVG from 'svgs/feature-4.svg?sprite';
-import FEATURE_5_SVG from 'svgs/feature-5.svg?sprite';
-import FEATURE_6_SVG from 'svgs/feature-6.svg?sprite';
-import FEATURE_7_SVG from 'svgs/feature-7.svg?sprite';
-import FEATURE_8_SVG from 'svgs/feature-8.svg?sprite';
+import ADDITIONAL_BENEFITS_SVG from 'svgs/features/additional-benefits.svg?sprite';
+import CARBON_MARKETS_SVG from 'svgs/features/carbon-markets.svg?sprite';
+import CASE_STUDIES_SVG from 'svgs/features/case-studies.svg?sprite';
+import CLIMATE_MITIGATION_SVG from 'svgs/features/climate-mitigation.svg?sprite';
+import ECONOMIC_FEASIBILITY_SVG from 'svgs/features/economic-feasibility.svg?sprite';
+import HUMAN_PERSPECTIVES_SVG from 'svgs/features/human-perspectives.svg?sprite';
+import POLICIES_SVG from 'svgs/features/policies.svg?sprite';
+import PROTOTYPING_NETWORK_SVG from 'svgs/features/prototyping-network.svg?sprite';
 
 export const FEATURES = [
   {
     key: '1',
-    description: 'Scientific data on all NCS pathways.',
-    icon: FEATURE_1_SVG,
+    title: 'Climate Mitigation Science',
+    icon: CLIMATE_MITIGATION_SVG,
+    description:
+      'We are developing the science to address two critical questions that must be answered to realize the potential of Natural Climate Solutions: Precisely where should we focus our efforts, and exactly what interventions have the highest likelihood of success? Research has shown how much nature can do: forests, agricultural lands, grasslands and wetlands can cost-effectively capture 11 billion metric tons of CO2e per year, a full third of the solution needed to stabilize our climate by 2030. We are covering a broad field of study: avoided forest conversion, climate-smart forestry, reforestation and agroforestry, agriculture and grasslands, blue carbon and peatlands. When complete, each playbook will include maps that show the largest climate mitigation opportunities per hectare of investment; operational protocols and methodologies to ensure projects achieve measurable climate mitigation; and on-the-ground research that fills critical knowledge gaps.',
   },
   {
     key: '5',
-    description: 'Information on positive impacts and other benefits to people and ecosystems.',
-    icon: FEATURE_5_SVG,
+    title: 'Carbon Markets and Standards',
+    icon: CARBON_MARKETS_SVG,
+    description:
+      'REDD+ implementers currently face a broad array of standards and financing options, some of which may be more appropriate depending on national or local circumstances. We seek to simplify the understanding of various standards, funding sources, and markets for REDD+, and to provide a centralized site for accessing International REDD+ Standards. We have assessed seven REDD+ standards and 30+ different types of requirements, broadly categorized under the following groups: carbon accounting, programmatic design, and social and environmental safeguards.',
   },
   {
     key: '2',
-    description: 'Policies in place at local and national levels.',
-    icon: FEATURE_2_SVG,
+    title: 'Additional Benefits',
+    icon: ADDITIONAL_BENEFITS_SVG,
+    description:
+      'Natural Climate Solutions (NCS) hold great promise for mitigating climate change. They also have the potential to deliver significant biodiversity and human well-being co-benefits, but we still lack an understanding of how the type and magnitude of these benefits vary by context. Systematically identifying and estimating co-benefits for priority NCS activities is critical for creating interest in, and promoting adoption of, policies and behaviors that accelerate NCS implementation across the world. The identification of NCS co-benefits allows proponents of NCS strategies to speak directly about how NCS can address multiple needs that go beyond climate change mitigation, which is crucial for engaging the many and often disparate decision-makers that are necessary for broadening NCS implementation. Decision-makers are frequently forced to examine tradeoffs between policies and programs that address diverse goals, such as increasing incomes of smallholder farmers; helping communities adapt to climate change; safeguarding biodiversity conservation in forests, rangelands, and coastal systems; and meeting nationally determined contributions to the Paris Agreement. NCS strategies must be prioritized and selected among competing strategies, and a full accounting of co-benefits is critical for informing the complex calculus decision-makers must make in the programs or policies that will be the focus of investments and policy attention.',
   },
   {
     key: '6',
-    description: 'Information on biomes and where the protected and conservation areas are.',
-    icon: FEATURE_6_SVG,
+    title: 'Case Studies',
+    icon: CASE_STUDIES_SVG,
+    description:
+      'Natural Climate Solutions are affordable, scalable, and available right now. Get a first glance at projects that protect, restore, and improve land management based on Natural Climate Solutions (NCS) and the broader Nature-based Solutions (NbS) – projects that increase carbon storage or avoid greenhouse gas emissions in forests, wetlands, grasslands, and agricultural lands.',
   },
   {
     key: '3',
-    description: 'Implementation costs. ',
-    icon: FEATURE_3_SVG,
+    title: 'Policies',
+    icon: POLICIES_SVG,
+    description:
+      'We have a unique opportunity to drive significant political commitment, regulations, and finances towards more sustainable food and land use systems. Governments have the power to unlock the incredible potential of Natural Climate Solutions (NCS) and Nature-based Solutions (NbS) in general. To create a nature positive economy, we must develop and implement frameworks that recognize the value of nature in decision-making, regulations, and market mechanisms. Nature4climate’s policy database bridges an important climate policy tracking gap. The database identifies effective government initiatives, particularly: budgets, legislation, and subsidies.',
   },
   {
     key: '7',
-    description: 'Guidance on high integrity projects and how to implement them.',
-    icon: FEATURE_7_SVG,
+    title: 'Prototyping Network',
+    icon: PROTOTYPING_NETWORK_SVG,
+    description:
+      'Building on the expertise of on-the-ground practitioners to help activate nature’s potential to limit climate change, we are developing a global NCS Prototyping Network to field test and evaluate high-impact NCS strategies that can be scaled and replicated around the world. The network brings together field staff and scientists from 15 existing TNC projects using NCS interventions in peatlands, coastal wetlands, and agroforestry systems. Each project is a means to field test the impact and efficacy of different approaches. Local project teams will work with the prototyping team to: <li>Measure mitigation and evaluate project impact.</li> <li>Assess other key factors relating to feasibility, such as costs, co-benefits to people and nature,</li> <li>and equitable implementation; and</li> <li>Identify challenges, conditions for success, and opportunities to scale.</li> The network will also provide a cross-project community of learning and collaboration, and a constant feedback loop to improve implementation through adaptive management. Data and case studies from prototype projects will be available to researchers and decision-makers around the world through the development of innovative tools and integrated into a set of playbooks for employing different NCS strategies.',
   },
   {
     key: '4',
-    description: 'Existing commitments, including NDCs and NAPs.',
-    icon: FEATURE_4_SVG,
+    title: 'Economic Feasibility',
+    icon: ECONOMIC_FEASIBILITY_SVG,
+    description:
+      'Carbon Potential and Feasibility - Scientists have been able to generate some high level data about the maximum potential of our natural landscapes to store carbon and mitigate climate change; what is less studied, however, are the economic and other real-world factors that may prevent land stewards from taking action, like the financial constraints of cash-strapped farmers, market forces that incentivize unsustainable practices, and policy and institutional barriers that work against implementation. Understanding these barriers and their impact on the adoption of NCS will help identify optimal solutions and guide decision-makers to target NCS investment to areas where interventions will achieve significant climate change mitigation at the lowest cost to society.',
   },
   {
     key: '8',
-    description: 'Inspiring stories and case studies.',
-    icon: FEATURE_8_SVG,
+    title: 'Human perspectives',
+    icon: HUMAN_PERSPECTIVES_SVG,
+    description:
+      'The NCS and NbS community of practice must take bold steps to ensure that projects not only avoid adverse human rights impacts but actively promote Indigenous People and Local Communities (IPLC) rights, self-determination, well-being, and equity, including full entitlement to benefits. The NCS community must strive to do this even though, as noted, the NCS concept is in the public domain, without a central authority to control what projects claim to involve NCS. The NCS Human Rights Toolset is designed to help scrutinize existing and proposed projects for risks of ongoing or potential future adverse impacts to human rights and IPLC self-determination. Because NCS is rooted in land use strategies (as opposed to industrial or consumer emissions reduction, for example) it offers a key platform for IPLCs to pursue their own self-determined climate agendas.',
   },
 ];
