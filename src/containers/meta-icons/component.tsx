a771901e2840ba9de96500ad84cb3b725cce00a7
anamontiaga <amontiaga@gmail.com>
2022-10-28 10:24:17 +0200

    fix meta-icons routes

--- src/containers/meta-icons/component.tsx
+++ src/containers/meta-icons/component.tsx
@@ -5,37 +5,77 @@ import Head from 'next/head';
 export const MetaIcons = () => {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="57x57" href="/meta-icons/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/meta-icons/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/meta-icons/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/meta-icons/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/meta-icons/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/meta-icons/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/meta-icons/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/meta-icons/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/meta-icons/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="36x36" href="/meta-icons/android-icon-36x36.png" />
-      <link rel="icon" type="image/png" sizes="48x48" href="/meta-icons/android-icon-48x48.png" />
-      <link rel="icon" type="image/png" sizes="72x72" href="/meta-icons/android-icon-72x72.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/meta-icons/android-icon-96x96.png" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/images/meta-icons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/images/meta-icons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/images/meta-icons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/images/meta-icons/apple-icon-76x76.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/meta-icons/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/meta-icons/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/meta-icons/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/meta-icons/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/meta-icons/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="36x36"
+        href="/images/meta-icons/android-icon-36x36.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href="/images/meta-icons/android-icon-48x48.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="72x72"
+        href="/images/meta-icons/android-icon-72x72.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/meta-icons/android-icon-96x96.png"
+      />
       <link
         rel="icon"
         type="image/png"
         sizes="144x144"
-        href="/meta-icons/android-icon-144x144.png"
+        href="/images/meta-icons/android-icon-144x144.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/meta-icons/android-icon-192x192.png"
+        href="/images/meta-icons/android-icon-192x192.png"
       />
-      <link rel="icon" type="image/png" sizes="32x32" href="/meta-icons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/meta-icons/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/meta-icons/favicon-16x16.png" />
-      <link rel="manifest" href="/meta-icons/manifest.json" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/meta-icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/images/meta-icons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/meta-icons/favicon-16x16.png" />
+      <link rel="manifest" href="/images/meta-icons/manifest.json" />
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/meta-icons/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/images/meta-icons/ms-icon-144x144.png" />
       <meta name="theme-color" content="#ffffff" />
     </Head>
   );
--- src/containers/meta-tags/component.tsx
+++ src/containers/meta-tags/component.tsx
@@ -29,14 +29,14 @@ export const MetaTags: React.FC<MetaTagsProps> = ({
       {publisher && <meta property="article:publisher" content={publisher} key="publisher" />}
       {section && <meta property="article:section" content={section} key="section" />}
       {tag && <meta property="article:tag" content={tag} key="tag" />}
-      <meta property="og:image" content={'images/social-media/share-image.png'} key="image" />
+      <meta property="og:image" content="images/social-media/share-image.png" key="image" />
       {imageURL && <meta property="og:image:secure_url" content={imageURL} key="imageURL" />}
       {imageWidth && <meta property="og:image:width" content={imageWidth} key="imageWidth" />}
       {imageHeight && <meta property="og:image:height" content={imageHeight} key="imageHeight" />}
       {twitterCard && <meta property="twitter:card" content={twitterCard} key="twitterCard" />}
       <meta
         property="twitter:image"
-        content={'images/social-media/twitter-image.png'}
+        content="images/social-media/twitter-image.png"
         key="twitterImage"
       />
       {twitterSite && <meta property="twitter:site" content={twitterSite} key="twitterSite" />}
