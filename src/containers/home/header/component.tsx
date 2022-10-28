d204ca67ef0939b0181ff008638a0288f746b915
anamontiaga <amontiaga@gmail.com>
2022-10-28 15:30:26 +0200

    set time out solution

--- src/containers/home/header/component.tsx
+++ src/containers/home/header/component.tsx
@@ -46,11 +46,12 @@ const Header: React.FC = () => {
 
     const yOffset = h ? $header.getBoundingClientRect().height : 0;
     const y = $scrollEl.getBoundingClientRect().top + window.pageYOffset + -yOffset;
-
-    window.scroll({
-      top: y,
-      behavior: h ? 'smooth' : 'auto',
-    });
+    setTimeout(() => {
+      window.scrollTo({
+        top: y,
+        behavior: h ? 'smooth' : 'auto',
+      });
+    }, 1000);
   }, []);
 
   return (
