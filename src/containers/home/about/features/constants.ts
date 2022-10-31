31f117ff595e8305150520169e344ba6b1106f80
anamontiaga <amontiaga@gmail.com>
2022-10-31 12:22:51 +0100

    reorder features

--- src/containers/home/about/features/constants.ts
+++ src/containers/home/about/features/constants.ts
@@ -16,25 +16,11 @@ export const FEATURES = [
       'We are developing the science to address two critical questions that must be answered to realize the potential of Natural Climate Solutions: Precisely where should we focus our efforts, and exactly what interventions have the highest likelihood of success? Research has shown how much nature can do: without technical constraints, forests, agricultural lands, grasslands and wetlands cost-effectively capture 11 billion metric tons of CO2e per year, a full third of the solution needed to stabilize our climate by 2030. We are covering a broad field of study: avoided forest conversion, climate-smart forestry, reforestation and agroforestry, regenerative agriculture, and protection or restoration of grasslands, peatlands and coastal wetlands. When complete, each of these content areas will provide maps that show the largest climate mitigation opportunities; operational protocols and methodologies to ensure projects achieve measurable climate mitigation; and on-the-ground research that fills critical knowledge gaps.',
   },
   {
-    key: '5',
-    title: 'Mitigation Costs',
-    icon: MITIGATION_COSTS_SVG,
-    description:
-      'Building on research to target NCS investments where interventions are most likely to achieve significant climate change mitigation at the lowest cost to society, Naturebase will demonstrate how much restoration, protection, and management activities could reduce climate change at various carbon market prices.  Using marginal abatement cost (MACC) analysis we can provide several pieces of useful information. For one, MACC curves indicate the total mitigation that can be achieved at up to any given unit cost ($ tCO2e). This is important for identifying projects whose unit cost of mitigation (i.e., MACC) is equal to or less than a given carbon price.  These MACC curves are useful because they allow comparisons among individual NCS activity and between NCS and other mitigation options, both with respect to mitigation potential and costs. In addition to facilitating comparison among mitigation options, information about the MACC of available mitigation options –both NCS and non-NCS– allows identification of the cost- effective mitigation portfolio for any desired total level of mitigation.',
-  },
-  {
     key: '2',
     title: 'Additional Benefits',
     icon: ADDITIONAL_BENEFITS_SVG,
     description:
       'Natural Climate Solutions (NCS) can also deliver significant “co-benefits” to biodiversity, adaptation, and human well-being. However, we still lack a full understanding of how these benefits vary by context and might change after implementing an NCS project. Systematically identifying evidence as well as estimating co-benefits for priority NCS pathways is critical for generating interest and promoting the adoption of policies that can accelerate NCS implementation. For example, understanding co-benefits can help convince decision-makers NCS are worth investing in. Or to understand tradeoffs when policies and programs seek to address diverse goals, such as increasing incomes of smallholder farmers; helping communities adapt to climate change; safeguarding biodiversity; and meeting nationally determined contributions to the Paris Agreement. A full accounting of NCS co-benefits is critical for informing the complex calculus decision-makers must make when investing in climate programs and advancing policy.',
-  },
-  {
-    key: '6',
-    title: 'Case Studies',
-    icon: CASE_STUDIES_SVG,
-    description:
-      'Natural Climate Solutions are affordable, scalable, and available right now. Get a first glance at projects that protect, restore, and improve land management based on Natural Climate Solutions (NCS) and the broader Nature-based Solutions (NbS).  See how science and information allows people to activate nature’s power to increase carbon storage and avoid greenhouse gas emissions in forests, wetlands, grasslands, and agricultural lands while preserving and enhancing biodiversity and human well-being.',
   },
   {
     key: '3',
@@ -44,6 +30,27 @@ export const FEATURES = [
       'We have a unique opportunity to drive significant political commitment, regulations, and finances towards more sustainable food and land use systems. Governments have the power to unlock the incredible potential of Natural Climate Solutions (NCS) and Nature-based Solutions (NbS) in general. To create a nature-positive economy, we must develop and implement frameworks that recognize the value of nature in decision-making, regulations, and market mechanisms. Nature4Climate’s policy database bridges an important climate policy tracking gap. The database identifies effective government initiatives, particularly: budgets, legislation, and subsidies and is designed to serve as a global database of public policies to support nature, helping governments and investors increase ambition and awareness of nature’s potential to tackle the climate emergency and biodiversity crisis more effectively.',
   },
   {
+    key: '4',
+    title: 'Human perspectives',
+    icon: HUMAN_PERSPECTIVES_SVG,
+    description:
+      'The NCS and NbS community of practice must take bold steps to ensure that projects not only avoid adverse human rights impacts but actively promote Indigenous People and Local Communities (IPLC) rights, self-determination, well-being, and equity, including full entitlement to benefits. The NCS community must strive to do this not only because it is the right thing to do, but also because it will ensure the long-term viability and sustainability of NCS projects. even though, as noted, the NCS concept is in the public domain, without a central authority to control what projects claim to involve NCS The NCS Human Rights Toolset is designed to help screen existing and proposed projects for risks of ongoing or potential future adverse impacts to human rights and IPLC self-determination. Because NCS is rooted in land use strategies (as opposed to industrial or consumer emissions reductions, for example) it offers a key platform for IPLCs to pursue their own self-determined climate agendas.',
+  },
+  {
+    key: '5',
+    title: 'Feasibility Constraints',
+    icon: FEASIBILITY_SVG,
+    description:
+      'Scientists have been able to generate high level data about the potential of natural landscapes to store carbon and mitigate climate change; what is less studied, however, are the economic and other real-world factors that may prevent land stewards from taking action. Constraints to NCS may include financial constraints of cash-strapped farmers, market forces that incentivize unsustainable practices, or informational, institutional, policy and governance barriers that work against implementation. Understanding where these constraints occur and how they relate to NCS implementation will help i guide decision-makers to target NCS investment where interventions are most likely to achieve significant climate change mitigation at the lowest cost to society. Importantly, it also will provide the information necessary for developing targeted interventions that can remove these barriers.',
+  },
+  {
+    key: '6',
+    title: 'Case Studies',
+    icon: CASE_STUDIES_SVG,
+    description:
+      'Natural Climate Solutions are affordable, scalable, and available right now. Get a first glance at projects that protect, restore, and improve land management based on Natural Climate Solutions (NCS) and the broader Nature-based Solutions (NbS).  See how science and information allows people to activate nature’s power to increase carbon storage and avoid greenhouse gas emissions in forests, wetlands, grasslands, and agricultural lands while preserving and enhancing biodiversity and human well-being.',
+  },
+  {
     key: '7',
     title: 'NCS Prototyping Network',
     icon: PROTOTYPING_NETWORK_SVG,
@@ -51,17 +58,10 @@ export const FEATURES = [
       'Building on the expertise of on-the-ground practitioners to help activate nature’s potential to limit climate change, we are developing a global NCS Prototyping Network to field test and evaluate high-impact NCS strategies that can be scaled and replicated around the world. The network brings together field staff and scientists from 15 existing TNC projects using NCS interventions in peatlands, coastal wetlands, and agroforestry systems. Each project is a means to field test the impact and efficacy of different approaches. Local project teams will work with the prototyping team to: <li>Measure mitigation and evaluate project impact.</li> <li>Assess other key factors relating to feasibility, such as costs, co-benefits to people, and nature.</li> <li>Equitable implementation.</li> <li>Identify challenges, conditions for success, and opportunities to scale.</li> The network will also provide a cross-project community of learning and collaboration, and a constant feedback loop to improve implementation through adaptive management. Data and case studies from pilot projects will be available to researchers and decision-makers around the world through the development of innovative tools and integrated into a set of playbooks for employing different NCS strategies.',
   },
   {
-    key: '4',
-    title: 'Feasibility Constraints',
-    icon: FEASIBILITY_SVG,
-    description:
-      'Scientists have been able to generate high level data about the potential of natural landscapes to store carbon and mitigate climate change; what is less studied, however, are the economic and other real-world factors that may prevent land stewards from taking action. Constraints to NCS may include financial constraints of cash-strapped farmers, market forces that incentivize unsustainable practices, or informational, institutional, policy and governance barriers that work against implementation. Understanding where these constraints occur and how they relate to NCS implementation will help i guide decision-makers to target NCS investment where interventions are most likely to achieve significant climate change mitigation at the lowest cost to society. Importantly, it also will provide the information necessary for developing targeted interventions that can remove these barriers.',
-  },
-  {
     key: '8',
-    title: 'Human perspectives',
-    icon: HUMAN_PERSPECTIVES_SVG,
+    title: 'Mitigation Costs',
+    icon: MITIGATION_COSTS_SVG,
     description:
-      'The NCS and NbS community of practice must take bold steps to ensure that projects not only avoid adverse human rights impacts but actively promote Indigenous People and Local Communities (IPLC) rights, self-determination, well-being, and equity, including full entitlement to benefits. The NCS community must strive to do this not only because it is the right thing to do, but also because it will ensure the long-term viability and sustainability of NCS projects. even though, as noted, the NCS concept is in the public domain, without a central authority to control what projects claim to involve NCS The NCS Human Rights Toolset is designed to help screen existing and proposed projects for risks of ongoing or potential future adverse impacts to human rights and IPLC self-determination. Because NCS is rooted in land use strategies (as opposed to industrial or consumer emissions reductions, for example) it offers a key platform for IPLCs to pursue their own self-determined climate agendas.',
+      'Building on research to target NCS investments where interventions are most likely to achieve significant climate change mitigation at the lowest cost to society, Naturebase will demonstrate how much restoration, protection, and management activities could reduce climate change at various carbon market prices.  Using marginal abatement cost (MACC) analysis we can provide several pieces of useful information. For one, MACC curves indicate the total mitigation that can be achieved at up to any given unit cost ($ tCO2e). This is important for identifying projects whose unit cost of mitigation (i.e., MACC) is equal to or less than a given carbon price.  These MACC curves are useful because they allow comparisons among individual NCS activity and between NCS and other mitigation options, both with respect to mitigation potential and costs. In addition to facilitating comparison among mitigation options, information about the MACC of available mitigation options –both NCS and non-NCS– allows identification of the cost- effective mitigation portfolio for any desired total level of mitigation.',
   },
 ];
