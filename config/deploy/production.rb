4a864106d91c65ca7a0168068759adcfee4cf4b9
Tiago Garcia <tiagojsag@gmail.com>
2023-04-26 13:59:16 +0200

    Modify deployment branch to main

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'ec2-18-207-144-211.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
-set :branch, 'deployment-config'
+set :branch, 'main'
