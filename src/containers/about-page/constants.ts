902842cd206b8493ef104c46edfd0d39bd4871e3
anamontiaga <amontiaga@gmail.com>
2023-10-31 12:31:19 +0100

    new faqs texts

--- src/containers/about-page/constants.ts
+++ src/containers/about-page/constants.ts
@@ -64,7 +64,7 @@ export const FAQS = [
   {
     question: 'What are natural climate solutions?',
     answer:
-      '<p>Natural climate solutions (NCS) are actions to <b>protect, better manage</b> and <b>restore</b> nature to reduce greenhouse gas emissions and store carbon. Science shows that— if deployed alongside other rapid decarbonisation efforts such as cutting fossil-fuel use and accelerating renewable energy—NCS can deliver a third of the emission reductions needed by 2030 to help us avoid the worst impacts of climate change.</p>',
+      '<p>Natural climate solutions (NCS) are actions to <b>protect, better manage</b> and <b>restore</b> nature to reduce greenhouse gas emissions and store carbon. Science shows that— if deployed alongside other rapid decarbonization efforts such as cutting out fossil-fuel use and accelerating renewable energy—nature can help us avoid the worst impacts of climate change and deliver a third of the emission reductions needed by 2030.</p>',
   },
   {
     question: 'How are natural climate solutions different from Nature-based solutions?',
@@ -74,28 +74,28 @@ export const FAQS = [
   {
     question: 'Who can use naturebase?',
     answer:
-      '<ul><li>a. Naturebase can serve <b>policymakers, practitioners and technical experts</b> on their journey to identify, analyze and make informed decisions on nature and climate planning and implementation. </li><li>b. It can also help <b>project managers</b> maximise their mitigation outcomes by combining different strategies and providing guidance on how to deliver high-integrity projects, including safeguarding human rights. </li><br/><li>c. For <b>Indigenous Peoples and local communities</b>, it can help attract direct finance for their invaluable work as stewards of nature, and elevate their voices, claims and stories by providing guidance on high-integrity NCS implementation, including a human rights screener, and inspiring case studies that showcase what meaningful engagement, safeguards and leadership looks like. </li><br/><li>d. For <b>governments</b>, naturebase works as a guide for better, more comprehensive nature and climate planning and delivery, as well as a tool to attract and direct finance on nature.</li><br/><li>e. <b>Funders</b>, on the other hand, can use naturebase as a first step to identify where and what to invest in, as well as learn more about the many benefits and returns investing in nature can provide.  </li></ul>',
+      '<ul><li>Naturebase can serve <b>policymakers, practitioners and technical experts</b> on their journey to identify, analyze and make informed decisions on nature and climate planning and implementation. </li> <br/><li>It can also help <b>project managers</b> maximise their mitigation outcomes by combining different strategies and providing guidance on how to deliver high-integrity projects that safeguard human rights. </li><br/><li>For <b>Indigenous Peoples and local communities</b>, it can help attract direct finance for their invaluable work as stewards of nature, and elevate their voices, claims and stories. Naturebase also provides guidance on high-integrity NCS implementation - including a human rights screener - and inspiring case studies that showcase what meaningful engagement, safeguards and leadership looks like. </li><br/><li>For <b>governments</b>, naturebase works as a guide for better, more comprehensive nature and climate planning and delivery, as well as a tool to attract and direct finance on nature.</li><br/><li><b>Funders</b>, on the other hand, can use naturebase as a first step to identify where and what to invest in, as well as learn more about the many benefits and returns investing in nature can provide.  </li></ul>',
   },
   {
-    question: 'What is available on the beta version?',
+    question: 'What features are currently available?',
     answer:
-      '<div><p>The full version of the naturebase platform is currently under development and this includes the full breadth of datasets. This beta version includes accurate data for 18 out of 25 natural climate solutions pathways and activities, including:</p><br/> <div><li>Reforestation</li><li>Avoided Forest Conversion</li> <li>Reduced Impact Logging for Climate Mitigation (RIL-C)</li><li>Reduced Woodfuel Harvest in Forests (RWF)</li><li>Reduced Woodfuel Harvest in Savannas (RWS)</li><li>Savanna burning</li><li>Increased Soil Carbon in Grazing Lands</li><li>Feed Management and Additives</li><li>Grassland Restoration (GrR)</li><li>Silvopasture</li><li>Trees in Croplands</li><li>Rice Cultivation</li><li>Cover Crops</li><li>Biochar</li><li>Reduced Tillage</li><li>Mangrove Protection</li><li>Mangrove Restoration</li><li>Peatland Restoration (PeR)</li></div><br/><p>Additional data on mitigation potential for avoided shrubland conversion (ASC), avoided grassland conversion (AGC), avoided peatland drainage (APC), seagrass protection, saltmarsh protection, seagrass restoration, and saltmarsh restoration is coming. This will affect global and national totals calculated for mitigation potential.</p></div>',
+      '<div><p> Naturebase will continue to evolve with new features and additional datasets. The current beta version includes accurate data for 18 out of 25 natural climate solutions pathways and activities, including: </p><br/> <div><li>Reforestation</li><li>Avoided Forest Conversion</li> <li>Reduced Impact Logging for Climate Mitigation (RIL-C)</li><li>Reduced Woodfuel Harvest in Forests (RWF)</li><li>Reduced Woodfuel Harvest in Savannas (RWS)</li><li>Savanna burning</li><li>Increased Soil Carbon in Grazing Lands</li><li>Feed Management and Additives</li><li>Grassland Restoration (GrR)</li><li>Silvopasture</li><li>Trees in Croplands</li><li>Rice Cultivation</li><li>Cover Crops</li><li>Biochar</li><li>Reduced Tillage</li><li>Mangrove Protection</li><li>Mangrove Restoration</li><li>Peatland Restoration (PeR)</li></div><br/><p>Additional data on mitigation potential for avoided shrubland conversion (ASC), avoided grassland conversion (AGC), avoided peatland drainage (APC), seagrass protection, saltmarsh protection, seagrass restoration, and saltmarsh restoration will be released soon. Whilst all pathway data available now is accurate to the indicated level of certainty, global and national totals for mitigation potential may increase as new pathways are included.</p></div>',
   },
   {
     question: 'How is naturebase different from other platforms?',
     answer:
-      'While many existing platforms serve the purpose of monitoring and reporting of progress around different natural ecosystems, such as the tracking of deforestation rates, naturebase focuses on identifying opportunities to protect, restore and sustainably manage nature to reduce the impacts of climate change. The platform offers users the largest database of peer-reviewed science on natural climate solutions, combined with additional resources such as co-benefits, policies, and financing. It also includes all identified activities for natural climate solutions at the global, national and subnational levels. Naturebase is designed not as a replacement but as an additional resource to supplement existing data and tools.',
+      'While many existing platforms serve the purpose of monitoring and reporting of progress around different natural ecosystems, such as the tracking of deforestation rates, naturebase focuses on identifying opportunities to protect, restore and sustainably manage nature to reduce the impacts of climate change. Our platform offers the largest database of peer-reviewed science on natural climate solutions, combined with additional resources such as co-benefits, policies, and financing. It also includes all identified activities for natural climate solutions at the global, national and subnational levels.',
   },
   {
     question:
       'How is naturebase inclusive of diverse voices and ensuring the tool aligns with the rights of Indigenous peoples and local communities?',
     answer:
-      'Indigenous People and local communities (IPLC) steward a quarter of the world’s lands and 80% of the world’s biodiversity. Naturebase intends to both respect and support these communities in their own pursuit of informed and self-determined climate and land use decision-making. The platform features a new NCS Human Rights Toolset specially designed to help screen existing and proposed projects for ongoing or potential future risks to human rights and agency. Further, sensitive data such as land tenure will not be publicly shared, conforming with principles of Free, Prior and Informed Consent (FPIC).',
+      '<div><p>Indigenous Peoples and local communities (IPLC) steward a quarter of the world’s lands and 80% of the world’s biodiversity. We recognize that IPLCs are the true stewards of lands and waters – and have been deploying natural climate solutions since time immemorial. Naturebase intends to both respect and support these communities in their own pursuit of informed and self-determined climate and land use decision-making. </p><br/><p>The platform includes the NCS Human Rights Toolset, which was specially designed to help screen existing and proposed projects for ongoing or potential future risks to human rights and agency.  Beyond that, sensitive data such as land tenure, as well as those uploaded onto the platform will not be publicly shared, conforming with principles of Free, Prior, and Informed Consent (FPIC).</p></div>',
   },
   {
-    question: 'Who is building this platform? Who owns it?',
+    question: 'Who is building this platform?',
     answer:
-      '<div><p>Naturebase is supported by the <a href="https://www.nature4climate.org" target="_blank"><Nature4Climate/a> coalition and developed by global experts at The Nature Conservancy, with contributions from numerous organizations across the environmental sector. As a free online tool that is built upon public peer-reviewed science and designed to serve the movement, naturebase does not intend to promote specific brands nor include any for-profit features. </p></div>',
+      '<div><p>Naturebase is supported by the <a href="https://www.nature4climate.org" target="_blank"><Nature4Climate/a> coalition and developed by global experts at The Nature Conservancy, Conservation International, the World Resources Institute,  the Global Mangrove Alliance and the Clean Cooking Alliance, with contributions from numerous other organizations across the environmental sector. As a free online tool that is built upon public transparent, open source, peer-reviewed science and designed to serve the movement, naturebase does not intend to promote specific brands nor include any for-profit features.</p></div>',
   },
   {
     question: 'Who is funding naturebase?',
@@ -104,16 +104,22 @@ export const FAQS = [
   },
   {
     question: 'Which data sources does naturebase use?',
-    answer: '',
+    answer:
+      'The specific methodology and input data used to calculate the mitigation potential will vary among pathways, but all adhere to the core principles of natural climate solutions. Each spatial datasets presented here estimate the mitigation potential of Natural Climate Solution activities derived from original geospatial analysis using the latest satellite and peer-reviewed datasets of current land systems, varied geospatial data, and carbon emissions/sequestration estimates informed by a review of peer-reviewed publications. For more information see our data download section.',
   },
   {
     question: 'How recent is the data and how often is it updated?',
     answer:
-      'Pathway data represent the latest and most accurate estimations of the mitigation potential associated with each dataset. These estimations may be subject to regular updates, which are influenced by various factors including advancements in global scientific knowledge, such as changes in emissions factors or available global extent data, and will be different for each dataset.',
+      'The pathway data represents the latest and most accurate estimations of the mitigation potential associated with each dataset. These estimations may be subject to updates, influenced by advancements in global scientific knowledge, changes in emissions factors or available global extent data, and will differ for each dataset.',
   },
   {
-    question: 'How are the numbers calculated?',
+    question: 'How do we express climate mitigation potential ?',
     answer:
-      'The methodology to calculate the mitigation potential will vary per pathway. However, each will include the combination of geospatial data combined with emissions estimations. Climate mitigation potential will either be expressed as increased sequestration for restoration pathways (usually expressed in units of metric tons of carbon sequestered per year), avoided loss based on historic conversion (usually expressed in units of metric tons of carbon per hectare) for the protect pathways, or the increased sequestration or reduced emissions due to improved management practices for the manage pathways.',
+      'Climate mitigation potential will either be expressed as increased sequestration for restoration pathways (usually expressed in units of metric tons of carbon dioxide equivalent sequestered per year), avoided loss based on historic conversion (usually expressed in units of metric tons of carbon dioxide equivalent per hectare) for the protect pathways, or the increased sequestration or reduced emissions due to a combination for improved management practices for the manage pathways.',
+  },
+  {
+    question: 'What is the baseline that’s being assumed for the pathway data?',
+    answer:
+      'The baseline used for each pathway is dependent on each specific dataset. Accounting horizons will be standardized for each pathway for the years 2035 and 2050. All data assumptions and caveats are indicated in the individual methodology sections and will vary per dataset. For more information on the pathways, consult the data download section.',
   },
 ];
