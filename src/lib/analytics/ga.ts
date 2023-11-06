641c02457a72ededc53f78423882097215a0af38
anamontiaga <amontiaga@gmail.com>
2023-11-06 17:14:01 +0100

    add gtm

--- src/lib/analytics/ga.ts
+++ src/lib/analytics/ga.ts
@@ -1,4 +1,5 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+export const GTM_TRACKING_ID = process.env.NEXT_PUBLIC_GTM_TRACKING_ID;
 
 // log the pageview with their URL
 export const GAPage = (url: string): void => {
