6d5d6cdafa67ec7526ca3859ded65fb54c312384
Alvaro <pursuance@gmail.com>
2024-06-28 11:17:29 +0200

    Add new stories. Replace Ghana for Indonesia in featured. Optimize images

--- src/containers/news-page/list/constants.ts
+++ src/containers/news-page/list/constants.ts
@@ -1,5 +1,6 @@
 import { Story } from 'types';
 
+// The first two country stories will be featured
 export const STORIES: Story[] = [
   {
     id: 1,
@@ -18,18 +19,17 @@ export const STORIES: Story[] = [
     media: ['Article'],
   },
   {
-    id: 2,
-    country: 'Ghana',
-    title: 'From Peru to Ghana: how technology is transforming community reforestation',
+    id: 6,
+    country: 'Indonesia',
+    title:
+      'Wetlands for livelihoods: how local communities thrive in protecting mangroves and peatlands in Borneo',
     description:
-      'Follow Peruvian activist Betty Rubio’s trip to Ghana to share her expertise in monitoring and reporting technologies for reforestation and native plant growing in the Bowkrom Community.',
-    image: 'peru-ghana.jpg',
-    pathway: 'croplands',
-    article:
-      'https://nature4climate.org/from-peru-to-ghana-how-technology-is-transforming-community-reforestation',
+      'Dedicated to restoring the Teluk Semanting Mangrove, the village embraces the mantra: "more mangroves, more blessings," as they plant Rhizophora seedlings. Indonesia holds the third-highest potential globally for carbon sequestration through Natural Climate Solutions. Protecting Indonesia\'s remaining wetland ecosystems is crucial for meeting greenhouse gas emissions goals by the end of the decade, all while serving as a vital source of income for local communities.',
+    image: 'indonesia.webp',
+    pathway: 'savana',
+    article: 'https://nature4climate.org/naturebase-indonesia',
     clip: null,
-    video: null,
-    category: 'News',
+    category: 'Stories from the ground',
     media: ['Article'],
   },
   {
@@ -44,6 +44,38 @@ export const STORIES: Story[] = [
     clip: null,
     video: null,
     category: 'Stories from the ground',
+    media: ['Article'],
+  },
+  {
+    id: 7,
+    country: null,
+    title:
+      'Introducing naturebase: a new tool to scale up natural climate solutions for reaching our global goals',
+    description:
+      'Knowing precisely why, where and how to put nature into action can help attract the much-needed funding and policy action that will enable successful implementation and scale-up of natural climate solutions across the globe. Naturebase  integrates an array of data and information acquired through various technologies, including satellite data analysis and land system modelling, as well as machine learning, to show exactly why, where and how.',
+    image: 'naturebase.webp',
+    pathway: 'forest',
+    article:
+      'https://nature4climate.org/naturebase-a-starting-point-to-unlock-the-power-of-nature/',
+    clip: null,
+    video: null,
+    category: 'News',
+    media: ['Article'],
+  },
+  {
+    id: 8,
+    country: null,
+    title:
+      'Natural Climate Solutions must be nature-based, sustainable, climate-additional, measurable, and equitable, new study finds.',
+    description:
+      'Natural Climate Solutions (NCS) play a role in both short- and long-term climate change mitigation together with vital benefits for biodiversity and people alike. Scientists across several organisations have now agreed on five principles that outline what and how these actions must be implemented.',
+    image: 'nature-climate-solutions.webp',
+    pathway: 'forest',
+    article:
+      'https://nature4climate.org/natural-climate-solutions-must-be-nature-based-sustainable-climate-additional-measurable-and-equitable-new-study-finds/',
+    clip: null,
+    video: null,
+    category: 'News',
     media: ['Article'],
   },
   {
@@ -62,29 +94,19 @@ export const STORIES: Story[] = [
     media: ['Article'],
   },
   // 4th DECEMBER 2023
+  // TODO: Missing link
   // {
   //   id: 5,
   //   country: 'Tanzania',
-  //   title: 'Nature and community-based solutions: how carbon revenues transformed land use and management in Tanzania',
+  //   title:
+  //     'Nature and community-based solutions: how carbon revenues transformed land use and management in Tanzania',
   //   description:
-  //     'Indigenous communities in Tanzania are making history by selling high-integrity carbon credits, recognizing their millennia-long dedication to preserving East Africa's forests. With 60% of the revenue managed by these communities, funds are reinvested in health, education, and local business development, showcasing the transformative impact of nature-positive initiatives.',
+  //     "Indigenous communities in Tanzania are making history by selling high-integrity carbon credits, recognizing their millennia-long dedication to preserving East Africa's forests. With 60% of the revenue managed by these communities, funds are reinvested in health, education, and local business development, showcasing the transformative impact of nature-positive initiatives.",
   //   image: 'test-1.jpg',
   //   pathway: 'croplands',
   //   article: 'https://nature4climate.org/naturebase-tanzania',
   //   clip: null,
   //   category: 'News',
-  //   media: ['Article'],
-  // },
-  // {
-  //   id: 6,
-  //   country: 'Indonesia',
-  //   title: 'Wetlands for livelihoods: how local communities thrive in protecting mangroves and peatlands in Borneo',
-  //   description: 'Dedicated to restoring the Teluk Semanting Mangrove, the village embraces the mantra: "more mangroves, more blessings," as they plant Rhizophora seedlings. Indonesia holds the third-highest potential globally for carbon sequestration through Natural Climate Solutions. Protecting Indonesia's remaining wetland ecosystems is crucial for meeting greenhouse gas emissions goals by the end of the decade, all while serving as a vital source of income for local communities.',
-  //   image: 'test-3.jpg',
-  //   pathway: 'savana',
-  //   article: 'https://nature4climate.org/naturebase-indonesia,
-  //   clip: null,
-  //   category: 'Stories from the ground',
   //   media: ['Article'],
   // },
 ];
